ff4750203855d1506d99251ecc3ef335b356fc04
IamNomadSauce <garsteckphotography@gmail.com>
2024-08-23 23:12:29 -0400

    add fills to database

--- db/db.go
+++ db/db.go
@@ -84,6 +84,8 @@ func Write_Order(orders []Order) { // Current Orders for all accounts
 	if err != nil {
 		fmt.Sprintf("Error connecting to db %v", err)
 	}
+	
+	defer db.Close()
 
 	_, err = db.Exec("DELECT FROM Orders;")
 	if err != nil {
@@ -91,7 +93,7 @@ func Write_Order(orders []Order) { // Current Orders for all accounts
 	}
 
 	insertQuery := `
-	INSERT INTO orders (orderID, productID, tradeType, side, time, exchange, marketcategory, price, size)
+	INSERT INTO orders (orderID, productID, tradetype, side, time, exchange, marketcategory, price, size)
 	VALUES(?,?,?,?,?,?,?,?,?);
 	`
 	for _, order := range orders {
@@ -103,8 +105,48 @@ func Write_Order(orders []Order) { // Current Orders for all accounts
 	}
 
 	fmt.Println(len(orders), "orders added to db")
-
 }
+
+type Fill struct {
+	EntryID		string
+	TradeID		string
+	OrderID		string
+	Time		int64
+	TradeType	string
+	Price		string	
+	Size		string
+	Side		string
+	Commission	string
+	ProductID	string
+	Exchange	string
+	MarketCategory	string
+}
+
+func Write_Fill(fills []Fill) {
+	fmt.Println("\n------------------------------\n Write Fills \n------------------------------\n")
+
+	db, _ := DBConnect()
+	defer db.Close()
+	
+	insertQuery := `
+	REPLACE INTO fills (entryID, tradeID, orderID, time, tradetype, price, size, side, commission, productid, exchange, marketcategory);
+	`
+
+	for _, fill := range fills {
+		_, err := db.Exec(insertQuery, fill.EntryID, fill.TradeID, fill.OrderID, fill.Time, fill.TradeType, fill.Price, fill.Size, fill.Side, fill.Commission, fill.ProductID, fill.Exchange, fill.MarketCategory)
+		if err != nil {
+			fmt.Sprintf("Error inserting fill: \n%v", err)
+		}
+	}
+
+	fmt.Println(len(fills), "fills added to db successfully")
+}
+
+
+
+
+
+
 
 
 
